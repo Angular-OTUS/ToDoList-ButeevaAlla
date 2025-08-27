50a9c1600578d2c0bec2150d399f41b2d6f4bab2
AllaBoo <a.v.buteeva@gmail.com>
2025-08-28 00:18:08 +0300

    ToDoList component added

--- ToDoList/src/app/app.ts
+++ ToDoList/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ToDoList } from './components/to-do-list/to-do-list'
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToDoList],
   templateUrl: './app.html',
   styleUrl: './app.less'
 })
 export class App {
-  protected readonly title = signal('ToDoList');
+  protected readonly title = signal('ToDoList-ButeevaAlla');
 }
---
+++ ToDoList/src/app/components/to-do-list/to-do-list.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-to-do-list',
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './to-do-list.html',
+  styleUrls: ['./to-do-list.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToDoList {
+  tasks = [
+    'Buy a new gaming laptop',
+    'Complete previous task',
+    'Create some angular app'
+  ]
+}
