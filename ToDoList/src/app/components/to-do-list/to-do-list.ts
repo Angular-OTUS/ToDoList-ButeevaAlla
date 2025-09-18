9c29272e66283570e429fb66e85993cb29f4b500
Ilya Platonov <iplatonov@solit-clouds.ru>
2025-09-18 19:51:39 +0300

    feat(ToDoList):homework3 - помощь + Отформатировал код + Добавил принудительное срабатывание стратегии обнаружения изменений(this.cdr.detectChanges())

--- ToDoList/src/app/components/to-do-list/to-do-list.ts
+++ ToDoList/src/app/components/to-do-list/to-do-list.ts
@@ -1,42 +1,59 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+
 import { ITask } from '../../shared-sripts/interfaces';
 import { ToDoListItem } from './to-do-list-item/to-do-list-item';
 
 @Component({
   selector: 'app-to-do-list',
-  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule, ToDoListItem],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ToDoListItem
+  ],
   templateUrl: './to-do-list.html',
-  styleUrls: ['./to-do-list.less', '../../shared-styles/mixins.less', '../../shared-styles/buttons.less'],
-  changeDetection: ChangeDetectionStrategy.Default,
+  styleUrls: [
+    './to-do-list.less',
+    '../../shared-styles/mixins.less',
+    '../../shared-styles/buttons.less'
+  ],
 })
 
-export class ToDoList implements OnInit {
-  tasks: ITask[] = [
+export class ToDoList {
+  public tasks: ITask[] = [
     { id: 0, text: 'Buy a new gaming laptop', },
     { id: 1, text: 'Complete previous task', },
     { id: 2, text: 'Create some angular app', },
   ]
-  newTask: string = '';
-  isLoading: boolean = true;
+  public newTask: string = '';
+  public isLoading: boolean = true;
 
-  delete(id: number): void {
+  constructor(private cdr: ChangeDetectorRef) {
+    this.changeVisiable();
+  }
+
+  public delete(id: number): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
-  add(newTask: string): void {
-    const nextId: number = Math.max(...this.tasks.map(task => task.id)) + 1;
-    this.tasks.push({ id: nextId, text: newTask, });
+  public add(newTask: string): void {
+    this.tasks.push(
+      { 
+        id: Math.max(...this.tasks.map(task => task.id)) + 1,
+        text: newTask, 
+      }
+    );
   }
 
-  ngOnInit() {
-    console.log(this.isLoading);
-    setTimeout(() => {
+  private changeVisiable(): void {
+        setTimeout(() => {
       this.isLoading = false;
-      console.log(this.isLoading);
-    }, 500);
+      this.cdr.detectChanges();
+    }, 1000);
   }
 }
