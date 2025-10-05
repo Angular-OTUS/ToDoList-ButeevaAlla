bef3ca307abd7f6165f53a0ccf8446f4be3b394b
AllaBoo <a.v.buteeva@gmail.com>
2025-10-05 19:29:46 +0300

    добавлен TextArea для description в каждый элемент списка

--- ToDoList/src/app/components/to-do-list/to-do-list.ts
+++ ToDoList/src/app/components/to-do-list/to-do-list.ts
@@ -5,7 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { timer } from 'rxjs';
 
-import { ITask, IButton } from '../../shared-sripts/interfaces';
+import { INewTask, ITask, IButton } from '../../shared-sripts/interfaces';
 import { ToDoListItem } from './to-do-list-item/to-do-list-item';
 import { ButtonComponent } from '../button-component/button-component';
 
@@ -20,39 +20,36 @@ import { ButtonComponent } from '../button-component/button-component';
     ButtonComponent,
   ],
   templateUrl: './to-do-list.html',
-  styleUrls: [
-    './to-do-list.less',
-    '../../shared-styles/mixins.less',
-  ],
+  styleUrls: ['./to-do-list.less', '../../shared-styles/mixins.less'],
 })
-
 export class ToDoList implements OnInit {
   public tasks: ITask[] = [
-    { id: 0, text: 'Buy a new gaming laptop' },
-    { id: 1, text: 'Complete previous task' },
-    { id: 2, text: 'Create some angular app' },
-  ]
-  public newTask = '';
+    { id: 0, text: 'Buy a new gaming laptop', description: '' },
+    { id: 1, text: 'Complete previous task', description: '' },
+    { id: 2, text: 'Create some angular app', description: '' },
+  ];
+  public newTask: INewTask = { text: '', description: '' };
   public isLoading: WritableSignal<boolean> = signal<boolean>(true);
-  public BtnParams: IButton = { title: 'Add task', class: 'button button_add td-form__button' };
+  public BtnParams: IButton = {
+    title: 'Add task',
+    class: 'button button_add td-form__button',
+  };
 
   public delete(id: number): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
-  public add(newTask: string): void {
-    this.tasks.push(
-      {
-        id: Math.max(...this.tasks.map(task => task.id)) + 1,
-        text: newTask,
-      },
-    );
+  public add(newTask: INewTask): void {
+    this.tasks.push({
+      id: Math.max(...this.tasks.map((task) => task.id)) + 1,
+      text: newTask.text,
+      description: newTask.description,
+    });
   }
 
   public ngOnInit(): void {
-    timer(500)
-      .subscribe(() => {
-        this.isLoading.set(false);
-      });
+    timer(500).subscribe(() => {
+      this.isLoading.set(false);
+    });
   }
 }
