5932dbecc388d700326f16acff83588c17344eec
AllaBoo <a.v.buteeva@gmail.com>
2025-10-06 13:05:03 +0300

    Реализован показ описания для каждой задачи по клику

--- ToDoList/src/app/components/to-do-list/to-do-list.ts
+++ ToDoList/src/app/components/to-do-list/to-do-list.ts
@@ -11,24 +11,31 @@ import { ButtonComponent } from '../button-component/button-component';
 
 @Component({
   selector: 'app-to-do-list',
-  imports: [
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    ToDoListItem,
-    ButtonComponent,
-  ],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule, ToDoListItem, ButtonComponent],
   templateUrl: './to-do-list.html',
   styleUrls: ['./to-do-list.less', '../../shared-styles/mixins.less'],
 })
 export class ToDoList implements OnInit {
   public tasks: ITask[] = [
-    { id: 0, text: 'Buy a new gaming laptop', description: '' },
-    { id: 1, text: 'Complete previous task', description: '' },
-    { id: 2, text: 'Create some angular app', description: '' },
+    {
+      id: 0,
+      text: 'Buy a new gaming laptop',
+      description:
+        'Quos ducimus officiis dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, eligendi. ',
+    },
+    {
+      id: 1,
+      text: 'Complete previous task',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, eligendi.',
+    },
+    {
+      id: 2,
+      text: 'Create some angular app',
+      description: 'Autem ratione culpa nulla ipsam nihil asperiores natus deleniti doloribus dolorem quibusdam.',
+    },
   ];
   public newTask: INewTask = { text: '', description: '' };
+  public selectedItemId: number | null = null;
   public isLoading: WritableSignal<boolean> = signal<boolean>(true);
   public BtnParams: IButton = {
     title: 'Add task',
@@ -36,10 +43,16 @@ export class ToDoList implements OnInit {
   };
 
   public delete(id: number): void {
+    this.selectedItemId = null;
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
+  public selectTask(id: number): void {
+    this.selectedItemId = id;
+  }
+
   public add(newTask: INewTask): void {
+    this.selectedItemId = null;
     this.tasks.push({
       id: Math.max(...this.tasks.map((task) => task.id)) + 1,
       text: newTask.text,
